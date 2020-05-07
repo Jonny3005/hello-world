60048dc3cbbd34faca3d620794b8ec8e1384e4b2
jonaslevis2020 <skjonny@rocketmail.com>
2020-05-07 17:31:44 +0100

    added an input test from user

--- testProject/src/testProject/TestClass.java
+++ testProject/src/testProject/TestClass.java
@@ -1,9 +1,15 @@
 package testProject;
 
+import java.util.Scanner;
+
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!!");
+
+		System.out.println("Please say something :");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println("You said : "+str);
 	}
 
 }
