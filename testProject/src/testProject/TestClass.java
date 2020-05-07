877dda6d3b75ce5b24d7884901c8297458d04354
jonaslevis2020 <skjonny@rocketmail.com>
2020-05-07 18:09:53 +0100

    added if statement

--- testProject/src/testProject/TestClass.java
+++ testProject/src/testProject/TestClass.java
@@ -9,6 +9,11 @@ public class TestClass {
 		System.out.println("Please say something :");
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
+		if (!str.isEmpty()) {
+			System.out.println("String");
+		} else {
+			System.out.println("Empty");
+		}
 		System.out.println("You said : "+str);
 		sc.close();
 	}
